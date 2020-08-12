f082df8dcadf0ce32b191c5aeb03fe760a666f6f
e-galata <dushirak@gmail.com>
2020-08-12 12:18:27 +0600

    create and fix some issues

--- pages/locators.py
+++ pages/locators.py
@@ -25,7 +25,8 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     ADD_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner>strong:nth-child(1)")
-    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "#messages div:first-child strong")
+    MESSAGE_ADDING_ABOUT = (By.CSS_SELECTOR, "#messages>div:first-child>strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
     MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
