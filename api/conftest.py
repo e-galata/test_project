ad9bc0b3bf269a57498d829b1659b6f71492c1c8
evgenijgalata <dushirak@gmail.com>
2025-03-21 15:19:00 +0500

    Modified fixtures sctructer, updated tests for works with

--- api/conftest.py
+++ api/conftest.py
@@ -1,17 +1,26 @@
 import pytest
 import requests
 import random
-from configuration import BASE_URL, GET_LIST_USERS
-from jsonschema_get import GET_LIST_USER_SCHEMA
+
+def _request_get_api(BASE_URL, GET_LIST_USERS, **kwargs):
+    for key, value in kwargs.items():
+        GET_LIST_USERS += f'/{value}'
+    return requests.get(url=BASE_URL+GET_LIST_USERS)
 
 @pytest.fixture
-def get_list_users():
-    response = requests.get(url=BASE_URL+GET_LIST_USERS)
-    return response
+def request_get_api():
+    return _request_get_api
+
+def _get_single_user_random(id_from, id_to):
+    return random.randint(id_from, id_to)
 
 @pytest.fixture
-def get_single_user():
-    r = requests.get(url=BASE_URL+GET_LIST_USERS+'/'+str(random.randint(1,12)))
-    json_data = 'data'
-    data_block = {"data": GET_LIST_USER_SCHEMA['properties'][json_data]}
-    return r, data_block, json_data
+def get_single_user_random():
+    return _get_single_user_random
+
+def _get_schema_from_data(schema, pathto, data):
+    return {"data": schema[pathto][data]}
+
+@pytest.fixture
+def get_schema_from_data():
+    return _get_schema_from_data
--- api/test_something.py
+++ api/test_something.py
@@ -1,22 +1,22 @@
 import pytest
+from configuration import BASE_URL, GET_LIST_USERS
 from jsonschema_get import GET_LIST_USER_SCHEMA
 from basetestclasses import Response
 from pydantic_schema_get import Get_List_Users, Get_List_Users_User
 
-def test_get_list_users(get_list_users):
+def test_get_list_users(request_get_api):
     (
-        Response(get_list_users)
+        Response(request_get_api(BASE_URL, GET_LIST_USERS))
         .assert_status_code(200)
         .validate_by_jsonschema(GET_LIST_USER_SCHEMA)
         .validate_by_pydantic(Get_List_Users)
     )
 
 @pytest.mark.random_user
-def test_get_single_user(get_single_user):
-    r, data_block, json_data = get_single_user
+def test_get_single_user(request_get_api, get_single_user_random, get_schema_from_data):
     (
-        Response(r, json_data)
+        Response(request_get_api(BASE_URL, GET_LIST_USERS, user = get_single_user_random(1,12)), 'data')
         .assert_status_code(200)
-        .validate_by_jsonschema(data_block)
+        .validate_by_jsonschema(get_schema_from_data(GET_LIST_USER_SCHEMA, 'properties', 'data'))
         .validate_by_pydantic(Get_List_Users_User)
     )
